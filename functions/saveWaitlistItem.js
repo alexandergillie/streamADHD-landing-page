cf60d5cb26a0c8f1496b43999168c2e523e41c7a
alexandergillie <alexander.gillie@gmail.com>
2023-04-13 13:19:01 -0400

    add package json

--- functions/saveWaitlistItem.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-    // Create a prepared statement with our query
-    console.log(context)
-    const ps = context.env.STREAM_DB.prepare('SELECT * from waitlist');
-    const data = await ps.first();
-  
-    return Response.json(data);
-  }
---
+++ functions/waitlist.js
@@ -0,0 +1,16 @@
+export async function onRequest(context) {
+    // Create a prepared statement with our query
+    console.log(context)
+    try{
+      const ps = context.env.STREAM_DB.prepare('SELECT * from waitlist');
+      const db = context.env.STREAM_DB
+
+      const data = await ps.first();
+      return Response.json(data);
+    }
+    catch (e) {
+      console.log(e)
+      return Response.json(e)
+    }
+  
+  }
