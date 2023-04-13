19a902a1dbad3e2b2ec1c201d4903db17368e1b5
alexandergillie <alexander.gillie@gmail.com>
2023-04-13 11:49:24 -0400

    fix

--- functions/saveWaitlistItem.js
+++ functions/saveWaitlistItem.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
     // Create a prepared statement with our query
     console.log(context)
-    const ps = context.env.STREAM_DB.prepare('SELECT * from users');
+    const ps = context.env.STREAM_DB.prepare('SELECT * from waitlist');
     const data = await ps.first();
   
     return Response.json(data);
