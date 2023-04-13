1a7c0968ef7d551637ae1cdb38af8fb29aac2ee3
alexandergillie <alexander.gillie@gmail.com>
2023-04-13 11:46:31 -0400

    save waitlist function

---
+++ functions/saveWaitlistItem.js
@@ -0,0 +1,8 @@
+export async function onRequest(context) {
+    // Create a prepared statement with our query
+    console.log(context)
+    const ps = context.env.STREAM_DB.prepare('SELECT * from users');
+    const data = await ps.first();
+  
+    return Response.json(data);
+  }
